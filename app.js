7bcedc08dbaf63ed53f910e5770d96b94fd9c929
Michael Vartan <michael.vartan@student.csulb.edu>
2014-07-30 00:31:03 -0700

    removed jquery dependencies in demo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 "use strict";
-$(function() {
+document.body.onload = function() {
   showCode()
 
   // create my object;  NOTE: JQueryObject.get() returns an array of elements
   var me = {
-    firstName:  VarBinding()        .bind($("[bind=firstName]") .get()),
-    middleName: VarBinding()        .bind($("[bind=middleName]").get()),
-    lastName:   VarBinding("Vartan").bind($("[bind=lastName]")  .get()),
+    firstName:  VarBinding()        .bind(getAllElementsWithAttributes({attribute:"bind",value:"firstName"})),
+    middleName: VarBinding()        .bind(getAllElementsWithAttributes({attribute:"bind",value:"middleName"})),
+    lastName:   VarBinding("Vartan").bind(getAllElementsWithAttributes({attribute:"bind",value:"lastName"})),
     getFullName: function() {
       var first, middle, last,
       nameParts = []
@@ -17,19 +17,60 @@ $(function() {
     }
   }
 
-  $("#nameform").submit(function(e){
+  document.getElementById("nameform").onsubmit = function(e){
     e.preventDefault()
-    $("#nameform button").text("Fetched | My name is "+me.getFullName())
-  })
+    document.getElementById("whatismyname").textContent = "Fetched | My name is "+me.getFullName()
+  }
 
-})  
+} 
 
 function showCode() {
-  $("code[src]").each(function(index,element) {
-    var fileName = element.getAttribute("src")
-    $.ajax({url:fileName}).done(function(content) {
-      element.textContent = content
-      hljs.highlightBlock(element)
-    })
-  })
+  var codeBlocks = getAllElementsWithAttributes({tag:"code",attribute:"src"})
+  showNextCode(codeBlocks,0);
+}
+function showNextCode(elements,index) {
+  var element = elements[index]
+  var fileName = element.getAttribute("src")
+  var e = element;
+  ajax(fileName,function(content) {
+
+    e.textContent = content
+    hljs.highlightBlock(e)
+    if(index+1<elements.length)
+      showNextCode(elements,index+1)
+  });
+}
+function ajax(url, callback) {
+  var request;
+  if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    request=new XMLHttpRequest();
+  } else {// code for IE6, IE5
+    request=new ActiveXObject("Microsoft.request");
+  }
+  request.onreadystatechange=function() {
+    if(request.readyState==4)
+      callback(request.responseText);
+  }
+  request.open("GET",url,true);
+  request.send();  
+}
+function getAllElementsWithAttributes(options)
+{
+
+  var matchingElements = [];
+  if(typeof options === 'string')
+    var attribute = options;
+  else
+    var attribute = options.attribute
+  var allElements = document.getElementsByTagName('*');
+  for (var i = 0, n = allElements.length; i < n; i++)
+  {
+
+    if (allElements[i].getAttribute(attribute) && (!options.value || options.value == allElements[i].getAttribute(attribute)) && (!options.tag || options.tag.toUpperCase() == allElements[i].tagName))
+    {
+      // Element exists with attribute. Add to array.
+      matchingElements.push(allElements[i]);
+    }
+  }
+  return matchingElements;
 }
